993b46916679a8f367c7a17d3148f3cd1176583f
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-09-11 17:29:14 +0200

    Add data.js who contains skills and projects

---
+++ data.js
@@ -0,0 +1,35 @@
+export default {
+    projects: [
+        {
+            title: "Organic",
+            skills: ["html5", "css3", "jquery", "php", "responsive design"],
+            year: "2014",
+            link: "http://www.organic-gazon.fr/"
+        },
+        {
+            title: "Portrait d'Ailleurs",
+            skills: ["html5", "css3", "jquery", "responsive design"],
+            year: "2015",
+            link: "http://www.portraitdailleurs-coiffure.fr/",
+            others: "Agence Web - Naviciel"
+        },
+        {
+            title: "Arthur & les Arbres",
+            skills: ["html5", "css3", "jquery", "responsive design", "bolt cms"],
+            year: "2015-2016",
+            link: "http://www.arthuretlesarbres.com/"
+        },
+        {
+            title: "Prochain projet",
+            skills: ["gatsby", "netlify cms"],
+            year: "2018"
+        },
+    ],
+    "skills": [
+        "javascript",
+        "vue.js",
+        "html",
+        "css",
+        "git"
+    ]
+}
